080e118ea9aa7a5003bc437ef0d16f4587fbe03d
GeraMG0809 <gerardo.mercado5055@alumnos.udg.mx>
2026-05-30 02:13:14 -0600

    restriccion y actualizacion de quinielas

--- src/app/api/predictions/route.ts
+++ src/app/api/predictions/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
 
 export async function POST(request: Request) {
-
   try {
-
     const body = await request.json()
 
     const {
@@ -14,8 +12,18 @@ export async function POST(request: Request) {
       awayScore
     } = body
 
-    const prediction = await prisma.prediction.create({
-      data: {
+    const prediction = await prisma.prediction.upsert({
+      where: {
+          userId_matchId: {
+          userId,
+          matchId
+        }
+      },
+      update: {
+        predictedHomeScore: homeScore,
+        predictedAwayScore: awayScore
+      },
+      create: {
         userId,
         matchId,
         predictedHomeScore: homeScore,
@@ -26,7 +34,6 @@ export async function POST(request: Request) {
     return NextResponse.json(prediction)
 
   } catch (error) {
-
     console.error(error)
 
     return NextResponse.json(
@@ -41,29 +48,11 @@ export async function POST(request: Request) {
 }
 
 export async function GET() {
+  const predictions = await prisma.prediction.findMany({
+    include: {
+      match: true
+    }
+  })
 
-  try {
-
-    const predictions = await prisma.prediction.findMany({
-      include: {
-        match: true,
-        user: true
-      }
-    })
-
-    return NextResponse.json(predictions)
-
-  } catch (error) {
-
-    console.error(error)
-
-    return NextResponse.json(
-      {
-        error: "Error al obtener predicciones"
-      },
-      {
-        status: 500
-      }
-    )
-  }
+  return NextResponse.json(predictions)
 }
